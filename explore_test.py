b021cb06844a7acfb50867c3f6b6657b04857fab
André Santiago <82945862+Dresovense@users.noreply.github.com>
2025-05-07 17:51:24 +0200

    hill_list_json

--- explore_test.py
+++ explore_test.py
@@ -9,8 +9,6 @@ with open("data\hill_list.txt", "r") as file:
 
 places_not_catched = 0
 clean = 0
-should_it_be_here = 0
-good_potentially = 0
 for place in hill_list:
     if place in NER_dictionnary.keys():
         clean += 1
@@ -18,13 +16,6 @@ for place in hill_list:
         places_not_catched += 1
         print(place)
 
-for place in NER_dictionnary.keys():
-    if "?" == place[0]:
-        should_it_be_here += 1
-    if "?" == place[-1]:
-        good_potentially += 1
 
 print(f"places_not_catched: {places_not_catched}")
 print(f"Clean: {clean}")
-print(f"should_it_be_here: {should_it_be_here}")
-print(f"good_potentially: {good_potentially}")
